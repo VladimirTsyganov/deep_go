0bf027b1757ad3df25b075090f112ece279fe83c
Vladimir Tsyganov <commanche@yandex.ru>
2025-07-02 13:51:16 +0300

    HW#1 data types

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+func ToLittleEndian[T uint16 | uint32 | uint64](number T) T {
+	size := unsafe.Sizeof(number)
+	tail := unsafe.Add(unsafe.Pointer(&number), size)
+
+	resultPtr := unsafe.Pointer(new(T))
+
+	offset := 0
+
+	for {
+		if offset >= int(size) {
+			break
+		}
+
+		readPtr := unsafe.Add(tail, -offset-1)
+		writePrt := (*uint8)(unsafe.Add(resultPtr, offset))
+		*writePrt = *(*uint8)(readPtr)
+		offset += 1
+	}
+
+	return *(*T)(resultPtr)
 }
 
-func TestСonversion(t *testing.T) {
+func TestСonversionUint32(t *testing.T) {
 	tests := map[string]struct {
 		number uint32
 		result uint32
@@ -36,6 +55,80 @@ func TestСonversion(t *testing.T) {
 		"test case #5": {
 			number: 0x01020304,
 			result: 0x04030201,
+		},
+		"test case #6": {
+			number: 0x01, // 0x00000001
+			result: 0x01000000,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestСonversionUint16(t *testing.T) {
+	tests := map[string]struct {
+		number uint16
+		result uint16
+	}{
+		"test case #1": {
+			number: 0x0000,
+			result: 0x0000,
+		},
+		"test case #2": {
+			number: 0xFFFF,
+			result: 0xFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #4": {
+			number: 0xF00F,
+			result: 0x0FF0,
+		},
+		"test case #5": {
+			number: 0x0104,
+			result: 0x0401,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestСonversionUint64(t *testing.T) {
+	tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"test case #1": {
+			number: 0x0000000000000000,
+			result: 0x0000000000000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFFFFFFFFFF,
+			result: 0xFFFFFFFFFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF00FF00FF,
+			result: 0xFF00FF00FF00FF00,
+		},
+		"test case #4": {
+			number: 0x00000000FFFFFFFF,
+			result: 0xFFFFFFFF00000000,
+		},
+		"test case #5": {
+			number: 0x01020304050607,
+			result: 0x0706050403020100,
 		},
 	}
 

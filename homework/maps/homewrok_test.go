57b09d2625c4664f6f77b1ce05a98bc1c531d60f
Vladimir Tsyganov <commanche@yandex.ru>
2025-07-29 23:43:41 +0300

    refactor

--- homework/maps/homewrok_test.go
+++ homework/maps/homewrok_test.go
@@ -9,15 +9,15 @@ import (
 
 // go test -v homework_test.go
 
-type TreeLeaf struct {
+type TreeNode struct {
 	key     int
 	value   int
-	lBranch *TreeLeaf
-	rBranch *TreeLeaf
+	lBranch *TreeNode
+	rBranch *TreeNode
 }
 
 type OrderedMap struct {
-	head *TreeLeaf
+	head *TreeNode
 	size int
 }
 
@@ -27,7 +27,7 @@ func NewOrderedMap() OrderedMap {
 
 func (m *OrderedMap) Insert(key, value int) {
 	if m.head == nil {
-		m.head = &TreeLeaf{
+		m.head = &TreeNode{
 			key:   key,
 			value: value,
 		}
@@ -35,73 +35,68 @@ func (m *OrderedMap) Insert(key, value int) {
 		return
 	}
 
-	el, built := findOrBuildLeaf(m.head, key)
+	var node *TreeNode
 
-	if built {
+	if !m.Contains(key) {
 		m.size += 1
+
+		node = m.insertNode(key)
+	} else {
+		node = findNode(m.head, key)
 	}
 
-	el.value = value
+	node.value = value
 }
 
 func (m *OrderedMap) Erase(key int) {
-	if m.size == 0 {
+	if m.empty() || !m.Contains(key) {
 		return
 	}
 
-	if m.head.key == key {
+	if m.size == 1 {
 		m.head = nil
 		m.size = 0
 		return
 	}
 
-	root, toDelete := m.findRootLeafToDelete(m.head, key)
-	if root == nil {
-		return
-	}
+	var replacement *TreeNode
 
-	var newBranch *TreeLeaf
+	root := findRootNode(m.head, key)
+	toDelete := findNode(root, key)
 
-	if toDelete.lBranch == nil && toDelete.rBranch == nil {
-		newBranch = nil
-	} else if toDelete.lBranch != nil && toDelete.rBranch != nil {
-		base := toDelete
-		minEl := toDelete.rBranch
+	if toDelete.lBranch == nil || toDelete.rBranch == nil {
 
-		for {
-			if minEl.lBranch == nil {
-				break
-			}
-
-			base = minEl
-			minEl = base.lBranch
+		if toDelete.lBranch != nil {
+			replacement = toDelete.lBranch
+		} else {
+			replacement = toDelete.rBranch
 		}
-
-		base.lBranch = minEl.rBranch
-		newBranch = minEl
-	} else if toDelete.lBranch != nil {
-		newBranch = toDelete.lBranch
 	} else {
-		newBranch = toDelete.rBranch
+		replacement = findMinAndDetach(toDelete.rBranch)
+		replacement.lBranch = toDelete.lBranch
+
+		if replacement != toDelete.rBranch {
+			replacement.rBranch = toDelete.rBranch
+		}
 	}
 
-	if root.key > key {
-		root.lBranch = newBranch
+	if root.lBranch == toDelete {
+		root.lBranch = replacement
 	} else {
-		root.rBranch = newBranch
+		root.rBranch = replacement
 	}
 
 	m.size -= 1
 }
 
 func (m *OrderedMap) Contains(key int) bool {
-	if m.size == 0 {
+	if m.empty() {
 		return false
 	}
 
-	leaf := m.find(m.head, key)
+	node := findNode(m.head, key)
 
-	return leaf != nil
+	return node != nil
 }
 
 func (m *OrderedMap) Size() int {
@@ -109,64 +104,27 @@ func (m *OrderedMap) Size() int {
 }
 
 func (m *OrderedMap) ForEach(action func(int, int)) {
-	if m.size == 0 {
+	if m.empty() {
 		return
 	}
 
 	m.applyAction(m.head, action)
 }
 
-func findOrBuildLeaf(from *TreeLeaf, key int) (*TreeLeaf, bool) {
-	var leaf *TreeLeaf
+func (m *OrderedMap) insertNode(key int) *TreeNode {
+	rootNode := findRootNode(m.head, key)
+	node := &TreeNode{key: key}
 
-	if from.key == key {
-		return from, false
-	}
-
-	if from.key > key {
-		leaf = from.lBranch
+	if key > rootNode.key {
+		rootNode.rBranch = node
 	} else {
-		leaf = from.rBranch
+		rootNode.lBranch = node
 	}
 
-	if leaf != nil {
-		return findOrBuildLeaf(leaf, key)
-	}
-
-	leaf = &TreeLeaf{
-		key: key,
-	}
-
-	if from.key > key {
-		from.lBranch = leaf
-	} else {
-		from.rBranch = leaf
-	}
-
-	return leaf, true
+	return node
 }
 
-func (m *OrderedMap) find(from *TreeLeaf, key int) *TreeLeaf {
-	var branch *TreeLeaf
-
-	if from.key == key {
-		return from
-	}
-
-	if from.key > key {
-		branch = from.lBranch
-	} else {
-		branch = from.rBranch
-	}
-
-	if branch == nil {
-		return nil
-	}
-
-	return m.find(branch, key)
-}
-
-func (m *OrderedMap) applyAction(from *TreeLeaf, action func(int, int)) {
+func (m *OrderedMap) applyAction(from *TreeNode, action func(int, int)) {
 	if from.lBranch != nil {
 		m.applyAction(from.lBranch, action)
 	}
@@ -178,22 +136,64 @@ func (m *OrderedMap) applyAction(from *TreeLeaf, action func(int, int)) {
 	}
 }
 
-func (m *OrderedMap) findRootLeafToDelete(from *TreeLeaf, key int) (*TreeLeaf, *TreeLeaf) {
-	var branch *TreeLeaf
+func (m *OrderedMap) empty() bool {
+	return m.Size() == 0
+}
 
-	if from.key < key {
-		branch = from.rBranch
-	} else {
+func findNode(from *TreeNode, key int) *TreeNode {
+	if from == nil {
+		return nil
+	}
+
+	if from.key == key {
+		return from
+	}
+
+	if from.key > key {
+		return findNode(from.lBranch, key)
+	}
+
+	return findNode(from.rBranch, key)
+}
+
+func findRootNode(from *TreeNode, key int) *TreeNode {
+	var branch *TreeNode
+
+	if from.key > key {
 		branch = from.lBranch
+	} else {
+		branch = from.rBranch
 	}
 
-	if branch == nil {
-		return nil, nil
-	} else if branch.key == key {
-		return from, branch
+	if branch == nil || branch.key == key {
+		return from
 	}
 
-	return m.findRootLeafToDelete(branch, key)
+	return findRootNode(branch, key)
+}
+
+func findMinAndDetach(from *TreeNode) *TreeNode {
+	if from.lBranch == nil {
+		return from
+	}
+
+	minParent := from
+	minEl := minParent.lBranch
+
+	for {
+		if minEl.lBranch == nil {
+			break
+		}
+
+		minParent = minEl
+		minEl = minParent.lBranch
+	}
+
+	if minEl.rBranch != nil {
+		minParent.lBranch = minEl.rBranch
+	}
+
+	return minEl
 }
 
 func TestCircularQueue(t *testing.T) {
@@ -238,5 +238,59 @@ func TestCircularQueue(t *testing.T) {
 		keys = append(keys, key)
 	})
 
+	assert.True(t, reflect.DeepEqual(expectedKeys, keys))
+}
+
+func TestCircularQueueWithCornerCases(t *testing.T) {
+	data := NewOrderedMap()
+	assert.Zero(t, data.Size())
+
+	data.Insert(10, 10)
+	data.Insert(12, 12)
+	data.Insert(11, 11)
+	data.Insert(20, 20)
+	data.Insert(18, 18)
+	data.Insert(19, 19)
+	data.Insert(22, 22)
+
+	assert.Equal(t, 7, data.Size())
+	assert.True(t, data.Contains(10))
+	assert.True(t, data.Contains(12))
+	assert.False(t, data.Contains(3))
+	assert.False(t, data.Contains(13))
+
+	var keys []int
+	expectedKeys := []int{10, 11, 12, 18, 19, 20, 22}
+	data.ForEach(func(key, _ int) {
+		keys = append(keys, key)
+	})
+
+	assert.True(t, reflect.DeepEqual(expectedKeys, keys))
+
+	data.Erase(12)
+
+	assert.Equal(t, 6, data.Size())
+	assert.False(t, data.Contains(12))
+	assert.True(t, data.Contains(18))
+	assert.True(t, data.Contains(19))
+	assert.True(t, data.Contains(20))
+	assert.True(t, data.Contains(22))
+
+	keys = nil
+	expectedKeys = []int{10, 11, 18, 19, 20, 22}
+	data.ForEach(func(key, _ int) {
+		keys = append(keys, key)
+	})
+
+	assert.True(t, reflect.DeepEqual(expectedKeys, keys))
+
+	data.Erase(19)
+	data.Erase(18)
+
+	keys = nil
+	expectedKeys = []int{10, 11, 20, 22}
+	data.ForEach(func(key, _ int) {
+		keys = append(keys, key)
+	})
 	assert.True(t, reflect.DeepEqual(expectedKeys, keys))
 }
